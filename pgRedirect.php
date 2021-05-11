4f96701c1b83037244de97ee7abeccbaf86910f7
ank237 <43108616+ank237@users.noreply.github.com>
2021-05-11 15:44:29 +0530

    Create pgRedirect.php

---
+++ pgRedirect.php
@@ -0,0 +1,54 @@
+<?php
+header("Pragma: no-cache");
+header("Cache-Control: no-cache");
+header("Expires: 0");
+
+require_once("./config_paytm.php");
+require_once("./encdec_paytm.php");
+
+$checkSum = "";
+$paramList = array();
+
+$ORDER_ID = $_POST["ORDER_ID"] ;
+$CUST_ID = "CUST001";
+$INDUSTRY_TYPE_ID = "WEB";
+$CHANNEL_ID = "WEB";
+$TXN_AMOUNT = $_POST["TXN_AMOUNT"];
+
+
+$CALLBACK_URL = "https://run-in-game.000webhostapp.com/pgResponse.php";
+
+
+$paramList["MID"] = PAYTM_MERCHANT_MID;
+$paramList["ORDER_ID"] = $ORDER_ID;
+$paramList["CUST_ID"] = $CUST_ID;
+$paramList["INDUSTRY_TYPE_ID"] = $INDUSTRY_TYPE_ID;
+$paramList["CHANNEL_ID"] = $CHANNEL_ID;
+$paramList["TXN_AMOUNT"] = $TXN_AMOUNT;
+$paramList["WEBSITE"] = PAYTM_MERCHANT_WEBSITE;
+$paramList["CALLBACK_URL"] = $CALLBACK_URL; 
+
+
+$checkSum = getChecksumFromArray($paramList,PAYTM_MERCHANT_KEY);
+
+?>
+<html>
+<body>
+    <center><h1>Please do not refresh this page...</h1></center>
+        <form method="post" action="<?php echo PAYTM_TXN_URL ?>" name="f1">
+        <table border="1">
+            <tbody>
+            <?php
+            foreach($paramList as $name => $value) {
+                echo '<input type="hidden" name="' . $name .'" value="' . $value . '">';
+            }
+            ?>
+            <input type="hidden" name="CHECKSUMHASH" value="<?php echo $checkSum ?>">
+            </tbody>
+        </table>
+        <script type="text/javascript">
+            document.f1.submit();
+        </script>
+    </form>
+</body>
+</html>
